5dd4ac6b959deba1dd810c26b3e505f7611f1795
Steve Yen <steve.yen@gmail.com>
2015-08-09 22:19:40 -0700

    initial framework for mcp command

---
+++ cmd/mcp/main.go
@@ -0,0 +1,148 @@
+//  Copyright (c) 2015 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the
+//  License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an "AS
+//  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+
+package main
+
+import (
+	"expvar"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"path"
+	"runtime"
+	"runtime/pprof"
+	"time"
+
+	log "github.com/couchbase/clog"
+
+	"github.com/couchbaselabs/cbgt"
+	"github.com/couchbaselabs/cbgt/cmd"
+)
+
+var VERSION = "v0.0.0"
+
+var expvars = expvar.NewMap("stats")
+
+func main() {
+	tags := []string{""}
+	container := ""
+	weight := 0
+	extras := ""
+	bindHttp := "mcp"       // Don't listen on ADDR:PORT.
+	register := "unchanged" // Don't list mcp in Cfg.
+	uuid := "mcp-uuid"
+	server := "."
+
+	flag.Parse()
+
+	if flags.Help {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	if flags.Version {
+		fmt.Printf("%s main: %s, data: %s\n", path.Base(os.Args[0]),
+			VERSION, cbgt.VERSION)
+		os.Exit(0)
+	}
+
+	if os.Getenv("GOMAXPROCS") == "" {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	mr, err := cbgt.NewMsgRing(os.Stderr, 1000)
+	if err != nil {
+		log.Fatalf("main: could not create MsgRing, err: %v", err)
+	}
+	log.SetOutput(mr)
+
+	log.Printf("main: %s started (%s/%s)",
+		os.Args[0], VERSION, cbgt.VERSION)
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	go dumpOnSignalForPlatform()
+
+	MainWelcome(flagAliases)
+
+	s, err := os.Stat(flags.DataDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if flags.DataDir == DEFAULT_DATA_DIR {
+				log.Printf("main: creating data directory, dataDir: %s",
+					flags.DataDir)
+				err = os.Mkdir(flags.DataDir, 0700)
+				if err != nil {
+					log.Fatalf("main: could not create data directory,"+
+						" dataDir: %s, err: %v", flags.DataDir, err)
+				}
+			} else {
+				log.Fatalf("main: data directory does not exist,"+
+					" dataDir: %s", flags.DataDir)
+				return
+			}
+		} else {
+			log.Fatalf("main: could not access data directory,"+
+				" dataDir: %s, err: %v", flags.DataDir, err)
+			return
+		}
+	} else {
+		if !s.IsDir() {
+			log.Fatalf("main: not a directory, dataDir: %s", flags.DataDir)
+			return
+		}
+	}
+
+	// If cfg is down, we error, leaving it to some user-supplied
+	// outside watchdog to backoff and restart/retry.
+	cfg, err := cmd.MainCfg("mcp", flags.CfgConnect, bindHttp,
+		register, flags.DataDir)
+	if err != nil {
+		if err == cmd.ErrorBindHttp {
+			log.Fatalf("%v", err)
+			return
+		}
+		log.Fatalf("main: could not start cfg, cfgConnect: %s, err: %v\n"+
+			"  Please check that your -cfg/-cfgConnect parameter (%q)\n"+
+			"  is correct and/or that your configuration provider\n"+
+			"  is available.",
+			flags.CfgConnect, err, flags.CfgConnect)
+		return
+	}
+
+	mgr := cbgt.NewManager(cbgt.VERSION, cfg, uuid,
+		tags, container, weight, extras, bindHttp,
+		flags.DataDir, server, nil)
+
+	mgr.Start(register)
+}
+
+func MainWelcome(flagAliases map[string][]string) {
+	flag.VisitAll(func(f *flag.Flag) {
+		if flagAliases[f.Name] != nil {
+			log.Printf("  -%s=%q\n", f.Name, f.Value)
+		}
+	})
+	log.Printf("  GOMAXPROCS=%d", runtime.GOMAXPROCS(-1))
+}
+
+func dumpOnSignal(signals ...os.Signal) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, signals...)
+	for _ = range c {
+		log.Printf("dump: goroutine...")
+		pprof.Lookup("goroutine").WriteTo(os.Stderr, 1)
+		log.Printf("dump: heap...")
+		pprof.Lookup("heap").WriteTo(os.Stderr, 1)
+	}
+}
---
+++ cmd/mcp/main_flags.go
@@ -0,0 +1,140 @@
+//  Copyright (c) 2014 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the
+//  License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an "AS
+//  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"sort"
+	"strings"
+)
+
+const DEFAULT_DATA_DIR = "data"
+
+type Flags struct {
+	CfgConnect string
+	DataDir    string
+	Help       bool
+	Version    bool
+}
+
+var flags Flags
+var flagAliases map[string][]string
+
+func init() {
+	flagAliases = initFlags(&flags)
+}
+
+func initFlags(flags *Flags) map[string][]string {
+	flagAliases := map[string][]string{} // main flag name => all aliases.
+	flagKinds := map[string]string{}
+
+	s := func(v *string, names []string, kind string,
+		defaultVal, usage string) { // String cmd-line param.
+		for _, name := range names {
+			flag.StringVar(v, name, defaultVal, usage)
+		}
+		flagAliases[names[0]] = names
+		flagKinds[names[0]] = kind
+	}
+
+	b := func(v *bool, names []string, kind string,
+		defaultVal bool, usage string) { // Bool cmd-line param.
+		for _, name := range names {
+			flag.BoolVar(v, name, defaultVal, usage)
+		}
+		flagAliases[names[0]] = names
+		flagKinds[names[0]] = kind
+	}
+
+	s(&flags.CfgConnect,
+		[]string{"cfgConnect", "cfg", "c"}, "CFG_CONNECT", "simple",
+		"connection string to a configuration provider/server"+
+			"\nfor clustering multiple nodes:"+
+			"\n* couchbase:http://BUCKET_USER:BUCKET_PSWD@CB_HOST:CB_PORT"+
+			"\n     - manages a cluster configuration in a couchbase"+
+			"\n       3.x bucket; for example:"+
+			"\n       'couchbase:http://my-cfg-bucket@127.0.0.1:8091';"+
+			"\n* simple"+
+			"\n     - intended for development usage, the 'simple'"+
+			"\n       configuration provider manages a configuration"+
+			"\n       for a single, unclustered node in a local"+
+			"\n       file that's stored in the dataDir;"+
+			"\n* metakv"+
+			"\n     - manages a cluster configuration in metakv store;"+
+			"\n       environment variable CBAUTH_REVRPC_URL needs to be set"+
+			"\n       for metakv; for example:"+
+			"\n       'export CBAUTH_REVRPC_URL=http://user:password@localhost:9000/mcp';"+
+			"\ndefault is 'simple'.")
+	s(&flags.DataDir,
+		[]string{"dataDir", "data"}, "DIR", DEFAULT_DATA_DIR,
+		"optional directory path for local data storage;"+
+			"\ndefault is '"+DEFAULT_DATA_DIR+"'.")
+	b(&flags.Help,
+		[]string{"help", "?", "H", "h"}, "", false,
+		"print this usage message and exit.")
+	b(&flags.Version,
+		[]string{"version", "v"}, "", false,
+		"print version string and exit.")
+
+	flag.Usage = func() {
+		if !flags.Help {
+			return
+		}
+
+		base := path.Base(os.Args[0])
+
+		fmt.Fprintf(os.Stderr, "%s: couchbase mcp\n", base)
+		fmt.Fprintf(os.Stderr, "\nUsage: %s [flags]\n", base)
+		fmt.Fprintf(os.Stderr, "\nFlags:\n")
+
+		flagsByName := map[string]*flag.Flag{}
+		flag.VisitAll(func(f *flag.Flag) {
+			flagsByName[f.Name] = f
+		})
+
+		flags := []string(nil)
+		for name := range flagAliases {
+			flags = append(flags, name)
+		}
+		sort.Strings(flags)
+
+		for _, name := range flags {
+			aliases := flagAliases[name]
+			a := []string(nil)
+			for i := len(aliases) - 1; i >= 0; i-- {
+				a = append(a, aliases[i])
+			}
+			f := flagsByName[name]
+			fmt.Fprintf(os.Stderr, "  -%s %s\n",
+				strings.Join(a, ", -"), flagKinds[name])
+			fmt.Fprintf(os.Stderr, "      %s\n",
+				strings.Join(strings.Split(f.Usage, "\n"),
+					"\n      "))
+		}
+
+		fmt.Fprintf(os.Stderr, "\nExamples:")
+		fmt.Fprintf(os.Stderr, examples)
+		fmt.Fprintf(os.Stderr, "\nSee also:"+
+			" http://github.com"+
+			"/couchbaselabs/cbgt/tree/master/cmd/mcp\n\n")
+	}
+
+	return flagAliases
+}
+
+const examples = `
+  Example where mcp's configuration is kept in a couchbase "cfg-bucket":
+    ./mcp -cfg=couchbase:http://cfg-bucket@CB_HOST:8091
+`
