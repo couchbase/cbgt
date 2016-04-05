befdc0894a58cd35a31c78c25a4e98e6c2ba50a7
Marty Schoch <marty.schoch@gmail.com>
2016-04-05 16:19:42 -0400

    MB-19035 - original fix broke standalone ui
    the original fix for MB-19035 broke error message display with
    standalone ui, this fixes just that aspect
    
    Change-Id: I0f580476ba5f06ed1056506e68347c731ddb20ff
    Reviewed-on: http://review.couchbase.org/62455
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Marty Schoch <marty.schoch@gmail.com>

--- rest/static/js/controllers.js
+++ rest/static/js/controllers.js
@@ -3,6 +3,13 @@
 /* Controllers */
 
 function errorMessage(errorMessageFull, code) {
+    if (code == 403 && typeof errorMessageFull == "object") {
+        var rv = errorMessageFull.message + ": ";
+        for (var x in errorMessageFull.permissions) {
+            rv += errorMessageFull.permissions[x];
+        }
+        return rv;
+    }
     console.log("errorMessageFull", errorMessageFull, code);
     var a = (errorMessageFull || (code + "")).split("err: ");
     return a[a.length - 1];
