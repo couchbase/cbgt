121f21d80582b89d7104ad619979575b426b2be0
Sreekanth Sivasankaran <sreekanth.sivasankaran@couchbase.com>
2022-01-25 16:05:55 +0530

    MB-50347 - Failover/rebalance stuck for more than 18 hrs
    Bringing back all the rebalance progress notifications
    during each of the orchestrator control signalling handshakes.
    It should induce some natural delay between each of the Cfg
    operations as well as would help prevent any accidental
    progress updates.
    
    Change-Id: I961e662604bb78f63e38699a4682db5bf3ac1db7
    Reviewed-on: https://review.couchbase.org/c/cbgt/+/169409
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Abhinav Dangeti <abhinav@couchbase.com>
    Tested-by: Sreekanth Sivasankaran <sreekanth.sivasankaran@couchbase.com>

--- rebalance/rebalance.go
+++ rebalance/rebalance.go
@@ -547,16 +547,10 @@ func (r *Rebalancer) rebalanceIndex(stopCh chan struct{},
 
 		r.Logf("     progress: %+v", progress)
 
-		// propagate the orchestrator progress for further triggering detailed
-		// rebalance progress computations only when it is absolute necessary,
-		// eg: upon any actual partition movement status related events.
-		if partitionSeqCatchUpInProgress(progress, lastProgress) {
-
-			r.progressCh <- RebalanceProgress{
-				Error:                firstErr,
-				Index:                indexDef.Name,
-				OrchestratorProgress: progress,
-			}
+		r.progressCh <- RebalanceProgress{
+			Error:                firstErr,
+			Index:                indexDef.Name,
+			OrchestratorProgress: progress,
 		}
 
 		numProgress++
@@ -578,17 +572,6 @@ func (r *Rebalancer) rebalanceIndex(stopCh chan struct{},
 	// TODO: Compute proper change response.
 
 	return true, firstErr
-}
-
-// partitionSeqCatchUpInProgress checks whether there's been any
-// progress in the physical partition movement since the last
-// iteration based on the orchestrator stats.
-func partitionSeqCatchUpInProgress(progress,
-	lastProgress blance.OrchestratorProgress) bool {
-	started := progress.TotMoverAssignPartition
-	finished := lastProgress.TotMoverAssignPartitionOk +
-		lastProgress.TotMoverAssignPartitionErr
-	return started > finished
 }
 
 // initPlansForRecoveryRebalance attempts to figure out whether the
