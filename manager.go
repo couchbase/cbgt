cf62c70a7d3564220ac1b2d10a609bf962bb7956
Thejas-bhat <thejas.orkombu@couchbase.com>
2026-05-07 15:18:07 -0700

    MB-71788: manager option for configuring minSamplesPerCentoid
    Change-Id: I05a734872795895956208aeeeb4fe10c06451001
    Reviewed-on: https://review.couchbase.org/c/cbgt/+/244731
    Tested-by: <thejas.orkombu@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: gautham <gautham.k@couchbase.com>

--- manager.go
+++ manager.go
@@ -354,6 +354,7 @@ type ClusterOptions struct {
 	CustomScriptQueriesEnabled         string `json:"customScriptQueriesEnabled"`
 	EnableLazyIndexUpdate              string `json:"enableLazyIndexUpdate"`
 	CollectionsLimitPerIndex           string `json:"collectionsLimitPerIndex"`
+	MinSamplesPerCentroid              string `json:"minSamplesPerCentroid"`
 
 	// For Scan Plus
 	ScanPlusFetchBucketWideSeqNos string `json:"scanPlusFetchBucketWideSeqNos"`
